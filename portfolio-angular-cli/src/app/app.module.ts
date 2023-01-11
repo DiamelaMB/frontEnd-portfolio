28b73400293917a25e87ec04eb2b88136431ce3d
Diamela <119958899+DiamelaMB@users.noreply.github.com>
2023-01-11 00:13:33 -0300

    uno

--- portfolio-angular-cli/src/app/app.module.ts
+++ portfolio-angular-cli/src/app/app.module.ts
@@ -2,10 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { LogoAPComponent } from './components/logo-ap/logo-ap.component';
+import { ContactoComponent } from './components/contacto/contacto.component';
+import { BannerComponent } from './components/banner/banner.component';
+import { AboutMeComponent } from './components/about-me/about-me.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LogoAPComponent,
+    ContactoComponent,
+    BannerComponent,
+    AboutMeComponent
   ],
   imports: [
     BrowserModule
